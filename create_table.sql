141736c6d59d01f211deab7b60f1685d09d36978
Shreyashipalkar <shreyashipalkar03@gmail.com>
2026-07-12 00:10:45 +0530

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,39 @@
+CREATE TABLE sales (
+
+Order_ID VARCHAR(20),
+
+Order_Date DATE,
+
+Ship_Date DATE,
+
+Customer_ID VARCHAR(20),
+
+Customer_Name VARCHAR(100),
+
+State VARCHAR(50),
+
+City VARCHAR(50),
+
+Region VARCHAR(50),
+
+Category VARCHAR(50),
+
+Sub_Category VARCHAR(50),
+
+Product_Name VARCHAR(200),
+
+Sales DECIMAL(10,2),
+
+Quantity INT,
+
+Discount DECIMAL(5,2),
+
+Profit DECIMAL(10,2),
+
+Shipping_Cost DECIMAL(10,2),
+
+Payment_Method VARCHAR(50),
+
+Rating DECIMAL(3,1)
+
+);
